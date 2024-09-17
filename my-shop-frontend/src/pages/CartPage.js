6bbfcec460c35bae4960159564d962679d750f02
zhushule <zhushule1996@gmail.com>
2024-09-17 13:24:30 -0400

    Remove unused addToCart function to fix ESLint warning

--- my-shop-frontend/src/pages/CartPage.js
+++ my-shop-frontend/src/pages/CartPage.js
@@ -4,18 +4,6 @@ import Cart from '../components/Cart';
 const CartPage = () => {
   const [cartItems, setCartItems] = useState([]);
 
-  const addToCart = (product) => {
-    setCartItems((prevItems) => {
-      const existingItem = prevItems.find((item) => item.id === product.id);
-      if (existingItem) {
-        return prevItems.map((item) =>
-          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
-        );
-      }
-      return [...prevItems, { ...product, quantity: 1 }];
-    });
-  };
-
   const removeFromCart = (product) => {
     setCartItems((prevItems) => prevItems.filter((item) => item.id !== product.id));
   };
